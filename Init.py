2084c606ce552dc39b70b4501e72f9564487ca65
Juan Rios <64570901+Jsrios29@users.noreply.github.com>
2021-06-30 17:01:55 -0400

    Update Init.py

--- Init.py
+++ Init.py
@@ -8,42 +8,52 @@ and validation data, initiates the RBM, trains the RBM, and classifies the
 validation data.
 """
 
+"""
+Known Issues:
+    2. why does gibbs sampling take so long
+    3. Why does validating the data take so long
+"""
+
 #%%
 
 from numpy import loadtxt
 import pandas as pd
 from dataLoader import dataLoader as dl
 from RBM import RBM
+import math
+import time
 
 
 #%%
 # 1. Declaring RBM parameters
 
-n = 50 # the number of hidden units
+n = 10 # the number of hidden units
 m = 27613 # the max number of visible units
 k = 2 # number of possible values for V
 T = 1 # number of gibbs sampling cycles
 
 batchSize = 1000 # number of samples per batch
 epochs = 50 # how many epochs to train for
-alpha = 0.01/batchSize # learning rate alpha
+alpha = 1/batchSize # learning rate alpha, default 0.01/batchSize
 
 #%%
 # 2. Loading the training and validation data
+#tic = time.perf_counter()
+#toc = time.perf_counter()
+#print(f"loaded val data in {toc - tic:0.4f} seconds")
 
 trainData = dl.loadData("trainData.txt")
 trainMap = dl.loadData("trainMap.txt")
-#valData = loadtxt("valData.txt", comments="#", delimiter=" ", dtype =int, unpack=False)
+#valData = dl.loadData("valData.txt")
+
+numBatches =  math.ceil(len(trainData)/batchSize)
 
 #%%
 
 # 3. initialize and train the model
 
-model = RBM(k,m,n)
+model = RBM(k,m,n, alpha)
+#model.validate(valData, trainData, trainMap)
+model.train(epochs, numBatches, batchSize,  trainData, trainMap, T)
 
 
-#%%
-count = 0
-for listElem in trainData:
-    count += len(listElem)                    
-print('Total Number of elements : ', count)
